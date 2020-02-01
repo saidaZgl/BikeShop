bf368b0ac7adc942c745f9ff6ad32893708e77e2
Saida Zghal <zghal.saida@orange.fr>
2020-02-01 21:19:51 +0100

    mise en place de variable de session

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,91 @@
 var express = require("express");
 var router = express.Router();
 
-var dataBike = [
+let dataBike = [
   {
-    name: "BIKE065",
+    name: "BIKO45",
     url: "/images/bike-1.jpg",
     price: 679
   },
   {
-    name: "ZOOK7",
+    name: "ZOOK07",
     url: "/images/bike-2.jpg",
+    price: 999
+  },
+  {
+    name: "TITANS",
+    url: "/images/bike-3.jpg",
     price: 799
   },
   {
-    name: "LIKO089",
-    url: "/images/bike-3.jpg",
-    price: 839
-  },
-  {
-    name: "GEWO",
+    name: "CEWO",
     url: "/images/bike-4.jpg",
-    price: 1206
-  },
-  {
-    name: "TITAN5",
-    url: "/images/bike-5.jpg",
-    price: 989
+    price: 1300
   },
   {
     name: "AMIG39",
-    url: "/images/bike-6.jpg",
-    price: 599
-  }
-];
-
-var dataCardBike = [
-  {
-    name: "BIKO45",
-    url: "/images/bike-1.jpg",
-    price: 679,
-    quantity: 1
+    url: "/images/bike-5.jpg",
+    price: 479
   },
   {
-    name: "ZOOK7",
-    url: "/images/bike-2.jpg",
-    price: 779,
-    quantity: 1
+    name: "LIK099",
+    url: "/images/bike-6.jpg",
+    price: 869
   }
 ];
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { dataBike });
+  if (req.session.dataCardBike == undefined) {
+    req.session.dataCardBike = [];
+  }
+  res.render("index", {
+    dataBike: dataBike
+  });
 });
 
 router.get("/shop", function(req, res, next) {
-  res.render("shop", { dataCardBike });
+  var alreadyExist = false;
+
+  for (var i = 0; i < req.session.dataCardBike.length; i++) {
+    if (req.session.dataCardBike[i].name == req.query.bikeNameFromFront) {
+      req.session.dataCardBike[i].quantity =
+        Number(req.session.dataCardBike[i].quantity) + 1;
+      alreadyExist = true;
+    }
+  }
+
+  if (alreadyExist == false) {
+    req.session.dataCardBike.push({
+      name: req.query.bikeNameFromFront,
+      url: req.query.bikeImageFromFront,
+      price: req.query.bikePriceFromFront,
+      quantity: 1
+    });
+  }
+
+  res.render("shop", {
+    dataCardBike: req.session.dataCardBike
+  });
+});
+
+router.get("/delete-shop", function(req, res, next) {
+  req.session.dataCardBike.splice(req.query.position, 1);
+
+  res.render("shop", {
+    dataCardBike: req.session.dataCardBike
+  });
+});
+
+router.post("/update-shop", function(req, res, next) {
+  let position = req.body.position;
+  let newQuantity = req.body.quantity;
+
+  req.session.dataCardBike[position].quantity = newQuantity;
+
+  res.render("shop", {
+    dataCardBike: req.session.dataCardBike
+  });
 });
 
 module.exports = router;
